e2fab86cdd692e69a386f77b7dee9defbb230dea
Mohamed Amine Iguiji <miguiji@e2r4p9.1337.ma>
2024-11-17 21:50:14 +0100

    Adding dn schema

--- transcadence/api/models.py
+++ transcadence/api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Chats(models.Model):
+    chatId = models.AutoField(primary_key=True)
+    user1_ID = models.IntegerField()
+    user2_ID = models.IntegerField()
 
-class msg(models.Model):
-    # sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+class Message(models.Model):
+    chatId = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
+    senderId = models.IntegerField()
+    msg = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True) 
