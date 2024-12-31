165f3331da72b7f5f32fbe595e65486357ff5f77
kvras <iguiji.etudes@gmail.com>
2024-12-31 20:06:51 +0000

    saving the new state

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db',
+        'USER': 'postgres',
+        'PASSWORD': 'PASS',
+        'HOST': 'postgresql',
+        'PORT': '5432'
     }
 }
+.
 
 
 # Password validation
