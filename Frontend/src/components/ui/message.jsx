e24877625a5b46ae36a77897ee4d6bb0c143d558
Lmakinaa <jaija.ismail@gmail.com>
2025-01-02 09:36:08 +0100

    handled word overflow in chat messgaes

--- Frontend/src/components/ui/message.jsx
+++ Frontend/src/components/ui/message.jsx
@@ -17,7 +17,7 @@ const Message = ({ message, messageId, isMine, user, avatar, time, userId }) => 
                 </Link>
             </Avatar>
             <div className="">
-                <div className={`rounded-lg px-3 py-2 max-w-md ${isMine ? "bg-primary/20" : "glass"}`}>
+                <div className={`rounded-lg px-3 py-2 max-w-md ${isMine ? "bg-primary/20" : "glass"} break-words overflow-wrap`}>
                     <p>{message}</p>
                 </div>
                 <p className={`text-xs text-gray-500 mt-0.5 ${isMine ? "text-right" : "text-left"}`}>{timeAgo}</p>
