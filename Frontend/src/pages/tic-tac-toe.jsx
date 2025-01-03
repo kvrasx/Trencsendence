a6a848a1f9ae0982d2b0c588e7b0413caaa2d753
Lmakinaa <jaija.ismail@gmail.com>
2025-01-03 17:31:16 +0100

    added tic_tac_toe game

--- Frontend/src/pages/tic-tac-toe.jsx
+++ Frontend/src/pages/tic-tac-toe.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { Card } from "@/components/ui/card";
-import local from '@/assets'
-
+import local from "@/assets/local.jpg"
+import remote from "@/assets/remote.jpg"
+import Spinner from "@/components/ui/spinner";
 
 export function TicTacToe() {
 
@@ -9,9 +10,13 @@ export function TicTacToe() {
     const [isMyTurn, setIsMyTurn] = useState(false);
     const [socket, setSocket] = useState(null);
     const [board, setBoard] = useState({});
+    const [waiting, setWaiting] = useState(false);
     const [started, setStarted] = useState(false);
+    const [winner, setWinner] = useState(null);
 
     useEffect(() => {
+        if (!waiting)
+            return;
         const newSocket = new WebSocket('ws://localhost:8000/ws/game/lobby/');
         newSocket.onopen = () => {
             console.log('WebSocket connection established');
@@ -47,11 +52,15 @@ export function TicTacToe() {
 
                 case "game_over":
                     // Trigger game end dialog
+                    setWinner(data.winner);
+                    console.log(data.winner);
+                    
                     document.getElementById("winner-dialog")?.click();
                     break;
 
                 case "game_start":
                     // Optional: Add any game start logic
+                    waiting && setStarted(true);
                     console.log("Game is ready to begin");
                     break;
 
@@ -64,7 +73,7 @@ export function TicTacToe() {
             newSocket.close()
         };
 
-    }, []);
+    }, [waiting]);
 
     const handleCellClick = (event) => {
         // Ensure it's the player's turn and the cell is not already occupied
@@ -110,26 +119,27 @@ export function TicTacToe() {
             <div className="flex gap-6">
 
                 <div className="p-5 flex-1 glass flex flex-row justify-center items-center h-[50vh]">
-                    {started ? (
-
-                        <Card className="p-5 bg-transparent border-gray-500">
-                            <div className="grid grid-cols-3 gap-5 m-auto w-full">{renderBoard()}</div>
-                        </Card>
+                    {waiting ? (
+                        started ? (
+                            <Card className="p-5 bg-transparent border-gray-500">
+                                <div className="grid grid-cols-3 gap-5 m-auto w-full">{renderBoard()}</div>
+                            </Card>
+                        ) : (
+                            <div className="flex flex-col items-center justify-center">
+                                <div className="text-3xl font-bold text-center mb-4 text-gray-300">Waiting for opponent...</div>
+                                <Spinner />
+                            </div>
+                        )
                     ) : (
                         <>
-                        <div className="grid grid-cols-2 gap-3">
-
-                            
-
-                            <Card className="glass p-6 space-y-6">
-                                <h2 className="text-center">Matchmaking Mode</h2>
-                                <button className="btn btn-primary w-full" onClick>Play Online</button>
-                            </Card>
-
-                        </div>
+                            <div onClick={(prev) => { setWaiting(true) }} className="cursor-pointer relative border-2 border-secondary rounded-lg overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
+                                <img src={remote} alt="Remote Matchmaking Mode" className="h-[600px] w-[250px] object-cover shadow-lg" />
+                                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-gray-300 text-3xl text-center font-bold" style={{ fontFamily: 'Roboto, sans-serif' }}>
+                                    <span className="px-6">Join a Game</span>
+                                </div>
+                            </div>
                         </>
                     )}
-
                 </div>
 
 
