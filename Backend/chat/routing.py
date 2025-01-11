1b498c1f614b612c5512b4edb06da93dfcefd4fb
kvras <iguiji.etudes@gmail.com>
2025-01-11 16:15:49 +0000

    removing files corrupted

--- Backend/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/chat/', ChatConsumer.as_asgi()),  # Define the WebSocket path
-    path('ws/chat/<str:room>/', ChatConsumer.as_asgi()),  # Define the WebSocket path
-]
--- Backend/user_management/views.py
+++
@@ -1,3 +0,0 @@
-from .viewset_match import MatchTableViewSet
-from .viewset_user import UserTableViewSet
-from .viewset_OTP import OTPViewSet
